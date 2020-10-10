5eb894a3cce3fa2fe96137b3e7e1e02f80fff9c5
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-10-09 23:36:16 -0300

    A terminar

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Projetil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            double pi = Math.PI;
+            double g = 9.80665;
+
+            double velo;
+            double angulo; 
+
+            Console.WriteLine("Entre com a velocidade, em m/s..: ");
+            velo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o ângulo, em graus....: ");
+            angulo = double.Parse(Console.ReadLine());
+
+            
+
+        }
+    }
+}
